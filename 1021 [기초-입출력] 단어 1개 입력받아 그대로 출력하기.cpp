bdbb9bd954e20ce8e015b65214f14ddd0aade74f
devlee988 <tjdcks0928@naver.com>
2020-03-08 00:12:48 +0900

    코드업 알고리즘연습

---
+++ 1021 [기초-입출력] 단어 1개 입력받아 그대로 출력하기.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	char a[51];
+
+	cin >> a;
+	cout << a;
+
+	return 0;
+}
---
+++ 1022 [기초-입출력] 문장 1개 입력받아 그대로 출력하기.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int x, y, m;
+	char a[51];
+	fgets(a, 50, stdin);
+
+	printf("%s", a);
+
+	return 0;
+}
