2038f885a968b96d918c568047bf55dc601347db
devlee988 <tjdcks0928@naver.com>
2020-03-11 23:10:54 +0900

    코드업 3문제

---
+++ 1030 [기초-데이터형] 정수 1개 입력받아 그대로 출력하기3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	long long int a;
+
+	cin >> a;
+	cout << a;
+
+	return 0;
+}
