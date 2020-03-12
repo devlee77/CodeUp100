14f71f023876676ac7882517bf83bfa1a635b2bf
devlee988 <tjdcks0928@naver.com>
2020-03-12 14:05:05 +0900

    코드업 3문제

---
+++ 1032 [기초-출력변환] 10진 정수 입력받아 16진수로 출력하기1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a;
+
+	cin >> a;
+	printf("%x", a);
+
+	return 0;
+}
---
+++ 1034 [기초-출력변환] 8진 정수 1개 입력받아 10진수로 출력하기.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a;
+
+	scanf("%o", &a);
+	cout << a;
+
+	return 0;
+}
