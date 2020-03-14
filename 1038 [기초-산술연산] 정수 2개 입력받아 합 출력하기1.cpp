cd6a6a02725c8dcfb7f12cfdfee6dfc7f2347d3e
devlee988 <tjdcks0928@naver.com>
2020-03-14 13:00:40 +0900

    코드업 3문제

---
+++ 1038 [기초-산술연산] 정수 2개 입력받아 합 출력하기1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	long long int a, b;
+
+	cin >> a >> b;
+	cout << a + b;
+	
+	return 0;
+}
---
+++ 1040 [기초-산술연산] 정수 1개 입력받아 부호 바꿔 출력하기.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a;
+
+	cin >> a;
+
+	cout << -a;
+
+	return 0;
+}
