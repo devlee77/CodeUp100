835cbb389f02d18ca7637d43739d1ca94bb2e12b
devlee988 <tjdcks0928@naver.com>
2020-03-17 23:17:48 +0900

    코드업 3문제

---
+++ 1047 [기초-비트시프트연산] 정수 1개 입력받아 2배 곱해 출력하기.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a;
+
+	cin >> a;
+	printf("%d", a << 1);
+
+
+
+	return 0;
+}
