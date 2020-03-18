44c739b3f2b082c4f66aef4175add8e057da94eb
devlee988 <tjdcks0928@naver.com>
2020-03-18 15:53:59 +0900

    코드업 3문제

---
+++ 1051 [기초-비교연산] 두 정수 입력받아 비교하기3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, b;
+
+	cin >> a >> b;
+
+	printf("%d", a <= b);
+
+
+
+	return 0;
+}
